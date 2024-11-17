0879405f9b7930f1e9cb3d89146425c7f0bb5ea1
PedroAMDC <125517381+PedroAMDC@users.noreply.github.com>
2024-11-17 16:08:29 -0400

    Aula 4 parte 2
    Criamos a rota e autenticação de logout e login

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,26 @@
 from flask import Flask
 from config import Config
+from flask_login import LoginManager
 
 from src.routes.AppRoutes import AppRoutes
 from src.routes.UsersRoutes import UsersRoutes
 
 from database import db
 
+from src.models.UsersModel import User
+
 def create_app(config_class=Config):
     app = Flask(__name__,template_folder="views/")
     app.config.from_object(config_class)
     db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'app_routes.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return db.one_or_404(db.select(User).filter_by(id=user_id))
 
     app.register_blueprint(AppRoutes, url_prefix='/')
 
