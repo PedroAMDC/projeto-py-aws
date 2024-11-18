58d072576586bec59d916e45d42c1f3eb0e9bbe2
PedroAMDC <125517381+PedroAMDC@users.noreply.github.com>
2024-11-18 00:06:32 -0400

    Aula 6 parte 2
    Colocamos o Jquery no projeto, refatoração de variaveis para consistência de nomenclatura. Criei a visualização, edição e apagar a postagem. Rotas e controladoras app e posts atualizados.

--- src/routes/PostsRoutes.py
+++ src/routes/PostsRoutes.py
@@ -4,4 +4,8 @@ from src.controllers.PostsController import create, update, read, delete
 
 PostsRoutes = Blueprint('posts_routes',__name__)
 
-PostsRoutes.route('create/', methods=['GET','POST'])(create)  
+PostsRoutes.route('create/', methods=['GET','POST'])(create) 
+PostsRoutes.route('read/<useraccount>/<postid>', methods=['GET'])(read)
+PostsRoutes.route('update/<useraccount>/<postid>', methods=['GET', 'POST'])(update)
+PostsRoutes.route('delete/<useraccount>/<postid>', methods=['GET'])(delete)
+
